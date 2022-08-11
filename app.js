6f0faf1fa4a101a1dac4e65b33b7d5cd217dfc65
Ldalvik <subaruforge@gmail.com>
2022-08-11 12:22:44 -0400

    Fixed text detection not working for all tunes

--- app.js
+++ app.js
@@ -13,10 +13,43 @@ new SubmissionStream(client, forzaOpenTunes).on('item', (post) => {
     let messageType = ""
     if (post.created_utc < BOT_START) return
     if (post.selftext_html.includes(data.fancyPantsTag)) {
+        console.log("Fancypants text detected.")
         messageType = data.fancyPantsWarning
     } else if (post.selftext_html.includes(data.tuneFormatterTag)) {
+        console.log("Good tune detected.")
         messageType = data.tuneFormatterText
     }
     post.reply(`${messageType}${data.staticMessage}${data.footer}`)
     console.log(post)
 })
+
+// const getAutoReply = async () => {
+//     let data = "(Empty data)"
+//     try {
+//         const response = await fetch('https://raw.githubusercontent.com/OPTN-Club/OPTN-reddit-bot/main/auto_reply_post.txt')
+//         if (!response.ok) {
+//             throw (new Error(`${response.status} (${response.statusText})`))
+//         }
+//         data = await response.text()
+//     } catch (error) {
+//         console.error(`Error in fetch: ${error.message}`)
+//     }
+//     return data
+// }
+
+
+// let savedFancyPantsPosts = ["whbc4b"]
+
+// setInterval(() => {
+//     for (let postId of savedFancyPantsPosts) {
+//         const submission = client.getSubmission(postId)
+//         submission.selftext_html.then(function (postBody) {
+//             if (postBody !== null && submission.edited &&
+//                 postBody.includes("<p><a href=\"https://optn.club/formatter\">View this tune on optn.club</a></p>")) {
+//                 console.log("Fixed!")
+//             } else {
+//                 console.log("Unfixed.")
+//             }
+//         })
+//     }
+// }, 10000)
